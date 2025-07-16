53f5df9557a5abe86fa01361387aa85b872d876f
mokka thriveni <thrivenimokka24@gmail.com>
2025-07-16 16:00:07 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header'
 import Sidebar from './components/Sidebar'
 import Home from './components/Home'
